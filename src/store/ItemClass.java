fe064b41da85bcd4045ecbd56d879474332dab83
matgut33 <mattgutkin@gmail.com>
2017-01-30 18:54:42 -0500

    Started SPVM Coding
    -Fixed ItemsClass.java formattring
    -Added Scanner for SPVM
    -Started inputs for adding, searching, and removing items

--- src/store/ItemClass.java
+++ src/store/ItemClass.java
@@ -14,50 +14,50 @@ public class ItemClass {
     private String description, buyer;
     private double price, salesprice;
 
-public ItemClass() {
-    description = "Unknown";
-    buyer = "Unknown";
-    price = 0;
-    salesprice = 0;
-}
+    public ItemClass() {
+        description = "Unknown";
+        buyer = "Unknown";
+        price = 0;
+        salesprice = 0;
+    }
 
-public void ItemClass(String d, String b, double p, double s) {
-    description = d;
-    buyer = b;
-    price = p;
-    salesprice = s;   
-}
+    public void ItemClass(String d, String b, double p, double s) {
+        description = d;
+        buyer = b;
+        price = p;
+        salesprice = s;   
+    }
 
-public void SetDescription(String d) {
-    description = d;
-}
+    public void SetDescription(String d) {
+        description = d;
+    }
 
-public String GetDescription() {
-    return description;
-}
+    public String GetDescription() {
+        return description;
+    }
 
-public void SetBuyer(String b) {
-    buyer = b;
-}
+    public void SetBuyer(String b) {
+        buyer = b;
+    }
 
-public String GetBuyer() {
-    return buyer;
-}
+    public String GetBuyer() {
+        return buyer;
+    }
 
-public void SetPrice(double p) {
-    price = p;
-}
+    public void SetPrice(double p) {
+        price = p;
+    }
 
-public double GetPrice() {
-    return price;
-}
+    public double GetPrice() {
+        return price;
+    }
 
-public void SetSalesprice(double s) {
-    salesprice = s;
-}
+    public void SetSalesprice(double s) {
+        salesprice = s;
+    }
 
-public double GetSalesprice() {
-    return salesprice;
-}
+    public double GetSalesprice() {
+        return salesprice;
+    }
 
 }
--- src/store/Store.java
+++ src/store/Store.java
@@ -6,6 +6,8 @@
 
 package store;
 
+import java.util.Scanner;
+
 /**
  * @authors Henry Dench [c], Matthew Gutkin, Sam Meyerowitz
  * @author Period 8
@@ -16,7 +18,10 @@ public class Store {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner r = new Scanner(System.in); //Used as main scanner for all inputs.
+        
+        System.out.print("Would you like to add, search for, or remove items? (A/S/R) ");
+        String input1 = r.nextLine();
     }
 
 }
