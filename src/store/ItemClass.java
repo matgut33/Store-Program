8d859d36251d271f9bfe6f737df3f924625d4548
matgut33 <mattgutkin@gmail.com>
2017-01-30 14:29:12 -0500

    Store Project

---
+++ src/store/ItemClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package store;
+
+/**
+ * @author Matthew Gutkin
+ * @author Period 8
+ */
+public class ItemClass {
+
+}
---
+++ src/store/StoreClass.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package store;
+
+/**
+ * @author Matthew Gutkin
+ * @author Period 8
+ */
+public class StoreClass {
+    private String location, owner, phone;
+    private String[] items = new String[4];
+    
+    public StoreClass() {
+        location = "Unknown";
+        owner = "Unknown";
+        phone = "Unknown";
+    }
+    
+    public StoreClass(String l, String o, String p) {
+        location = l;
+        owner = o;
+        phone = p;
+    }
+    
+    public void setLocation(String l) {
+        location  = l;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+    
+    public void setOwner(String o) {
+        owner = o;
+    }
+    
+    public String getOwner() {
+        return owner;
+    }
+    
+    public void setPhone(String p) {
+        phone = p;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+}
